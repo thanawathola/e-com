7e129d014996363d16a2d7459cfe00a9643a6a1a
Thanawat Rungrueangphawan <thanawat.rrpw@gmail.com>
2024-11-08 10:33:47 +0700

    justify top content

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-red-300">Hello</h1>
-    </div>
+    <main className="flex flex-col min-h-screen">
+      <div className="flex justify-between gap-2 p-5 min-w-full bg-slate-800 fixed">
+        <div className="bg-red-400">
+          <h1 className="text-white px-36">Hello Contact here 012-345-6789</h1>
+        </div>
+        <div className="bg-red-400">
+          <h1 className="">About us | Join us</h1>
+        </div>
+      </div>
+      <div className="flex flex-col min-h-screen gap-2 p-10 max-w-xs">
+        <div>
+          <h2>HOLA</h2>
+        </div>
+        <div>Amigo</div>
+        <div>Que pasa</div>
+        <div>Que pasa</div>
+        <div>Que pasa</div>
+        <div>Que pasa</div>
+        <div>Que pasa</div>
+        <div>Que pasa</div>
+        <div>Que pasa</div>
+      </div>
+    </main>
   );
 }
