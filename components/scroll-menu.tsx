b44cb1f7338be1e0598564ae9215477121cdd2b4
Thanawat Rungrueangphawan <thanawat.rrpw@gmail.com>
2024-12-03 13:07:36 +0700

    added border for scroll menu

--- components/scroll-menu.tsx
+++ components/scroll-menu.tsx
@@ -3,22 +3,22 @@ import React from "react";
 const ScrollableMenu: React.FC = () => {
   return (
     <div className="flex-grow-0 flex-shrink-0 basis-[300px] overflow-y-scroll border border-gray-300 p-4 bg-gray-900 text-white">
-      <div className="py-2 px-3 m-2">Gaming Gear</div>
-      <div className="py-2 px-3 m-2">Item 1</div>
-      <div className="py-2 px-3 m-2">Item 2</div>
-      <div className="py-2 px-3 m-2">Item 3</div>
-      <div className="py-2 px-3 m-2">Item 4</div>
-      <div className="py-2 px-3 m-2">Item 5</div>
-      <div className="py-2 px-3 m-2">Item 6</div>
-      <div className="py-2 px-3 m-2">Item 7</div>
-      <div className="py-2 px-3 m-2">Item 8</div>
-      <div className="py-2 px-3 m-2">Item 9</div>
-      <div className="py-2 px-3 m-2">Item 10</div>
-      <div className="py-2 px-3 m-2">Item 11</div>
-      <div className="py-2 px-3 m-2">Item 12</div>
-      <div className="py-2 px-3 m-2">Item 13</div>
-      <div className="py-2 px-3 m-2">Item 14</div>
-      <div className="py-2 px-3 m-2">Item 15</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Gaming Gear</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 1</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 2</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 3</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 4</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 5</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 6</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 7</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 8</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 9</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 10</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 11</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 12</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 13</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 14</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 15</div>
     </div>
   );
 };
