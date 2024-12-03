cc0161bd4074f2c6cf8f74464a89d050f645f390
Thanawat Rungrueangphawan <thanawat.rrpw@gmail.com>
2024-12-03 13:41:38 +0700

    enough menu for scroll menu

---
+++ components/r-chevron.tsx
@@ -0,0 +1,11 @@
+import { FaChevronRight } from "react-icons/fa";
+
+const ChevronRight = () => {
+  return (
+    <div className="p-2">
+      <FaChevronRight className="text-blue-700 w-4 h-4" /> {/* Shopping cart icon */}
+    </div>
+  );
+};
+
+export default ChevronRight;
--- components/scroll-menu.tsx
+++ components/scroll-menu.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import ChevronRight from "./r-chevron";
 
 const ScrollableMenu: React.FC = () => {
   return (
     <div className="flex-grow-0 flex-shrink-0 basis-[300px] overflow-y-scroll border border-gray-300 p-4 bg-gray-900 text-white">
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Gaming Gear</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 1</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 2</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 3</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 4</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 5</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 6</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 7</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 8</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 9</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 10</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 11</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 12</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 13</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 14</div>
-      <div className="py-2 px-3 m-2 border-b border-gray-600">Item 15</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600 flex items-center gap-2">
+        <div className="border rounded-full bg-white">
+          <ChevronRight />
+        </div>
+        <div>Gaming Gear</div>
+      </div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Playstation</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Xbox</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Joy Stick</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Speaker </div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Keyboard</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Mouse</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Mouse Pad</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Headphone</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">In-ear</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Microphone</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Gaming Chair</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">Glasses</div>
+      <div className="py-2 px-3 m-2 border-b border-gray-600">PC Desk</div>
     </div>
   );
 };
